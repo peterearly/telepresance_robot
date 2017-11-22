b1a6c7f86318d3dbc9f8a9f597f9a310e7f2e538
peterearly <peterearly10@gmail.com>
2017-11-22 10:56:39 -0500

    Add files via upload

---
+++ sketch_nov22b/sketch_nov22b.ino
@@ -0,0 +1,72 @@
+#include "Wire.h"
+extern "C" { 
+#include "utility/twi.h"  // from Wire library, so we can do bus scanning
+}
+#include "Adafruit_VL53L0X.h"
+
+Adafruit_VL53L0X lox = Adafruit_VL53L0X(); 
+#define TCAADDR 0x70
+ 
+void tcaselect(uint8_t i) {
+  if (i > 7) return;
+ 
+  Wire.beginTransmission(TCAADDR);
+  Wire.write(1 << i);
+  Wire.endTransmission();  
+}
+ 
+ 
+// standard Arduino setup()
+void setup()
+{
+  
+    while (!Serial);
+    delay(1);
+ 
+   
+    
+    Serial.begin(115200);
+    Serial.println(F("VL53L0X API Simple Ranging example\n\n")); 
+    Serial.println("\nTCAScanner ready!");
+     Wire.begin();
+    for (uint8_t t=0; t<8; t++) {
+      tcaselect(t);
+      Serial.print("TCA Port #"); Serial.println(t);
+  
+      for (uint8_t addr = 0; addr<=127; addr++) {
+        if (addr == TCAADDR) continue;
+      
+        uint8_t data;
+        if (! twi_writeTo(addr, &data, 0, 1, 1)) {
+           Serial.print("Found I2C 0x");  Serial.println(addr,HEX);
+           
+        }
+      }
+    }
+    Serial.println("\ndone");
+}
+ 
+void loop() 
+{
+  for (uint8_t x = 1; x < 5; x++) {
+    
+ tcaselect(x);
+ if (!lox.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while(1);
+  }
+  VL53L0X_RangingMeasurementData_t measure;
+    
+  //Serial.print("Reading a measurement... ");
+  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+
+  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+    Serial.print("sensor "); Serial.print(x);
+    Serial.print(" Distance (mm): "); Serial.println(measure.RangeMilliMeter);
+  } else {
+    Serial.println(" out of range ");
+  }
+    
+  }
+}
+
